f701394caad9058bc7101e93240a9b73a4f78853
Jignesh-ubix <pritmanvar1109@gmail.com>
2025-04-01 12:31:09 +0530

    added body in msgs.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # from main import run_job
 from functions.threads_api import post_thread_with_text
@@ -12,7 +13,10 @@ app = FastAPI()
 # @app.get("/run_job")
 # def handle_job_running():
 #     return run_job()
+class PostContent(BaseModel):
+    text: str
+
 
 @app.post("/post_thread")
-def handle_job_running(text: str):
-    return post_thread_with_text(text)
+def handle_job_running(content: PostContent):
+    return post_thread_with_text(content.text)
