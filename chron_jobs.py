f96f7c2a6fdfcc913e0c7e303a1c084c7027b4bb
Jignesh-ubix <pritmanvar1109@gmail.com>
2025-04-01 11:08:25 +0530

    resolved merge.

--- chron_jobs.py
+++ chron_jobs.py
@@ -4,9 +4,11 @@ import time
 from main import run_job
 
 def call_api():
-    url = "http://localhost:8000/run_job"  # Replace with your API endpoint
+    url = "http://localhost:8686/run_job"  # Replace with your API endpoint
     print("RUNNING JOB")
+    print("STARTED JOB AT:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     response = requests.get(url)
+    print("COMPLETED JOB AT:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     try:
         if response.status_code == 200:
             print(response.json())
@@ -14,8 +16,8 @@ def call_api():
             print(f"Failed to fetch data: {response.status_code}")
     except Exception as e:
         print(f"Failed to fetch data: {e}")
-        
+
 if __name__ == "__main__":
     while True:
-        run_job()
+        call_api()
         time.sleep(1800)
--- functions/threads_api.py
+++ functions/threads_api.py
@@ -10,7 +10,7 @@ def post_thread_with_text(text):
     print(os.getenv('TH_ACCESS_TOKEN'))
     print(certifi.where())
     container_creation_url = f"https://graph.threads.net/v1.0/{os.getenv('TH_USER_ID')}/threads"
-    container_params = {"media_type": "TEXT", "text": text, "access_token": os.getenv("TH_ACCESS_TOKEN")}
+    container_params = {"media_type": "TEXT", "text": text.replace("\\n", "\n"), "access_token": os.getenv("TH_ACCESS_TOKEN")}
     container_response = requests.post(container_creation_url, params=container_params, verify=certifi.where()).json()
     print("Container creation", container_response)
 
@@ -22,3 +22,15 @@ def post_thread_with_text(text):
     return post_response['id']
 
 # post_thread_with_text("Hello, Threads! Testing without ngrok.")
+def post_thread_with_text_api(text):
+    url = f"https://stocknews-1.onrender.com/post_thread?text={'\\n'.join(text.splitlines())}"
+    headers = {
+        "accept": "application/json"
+    }
+
+    response = requests.post(url, headers=headers, data="")
+
+    print(response.status_code)
+    print(response)  # Assuming the response is in JSON format
+    
+    return response
