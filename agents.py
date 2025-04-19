989977126618abe277f64f28b79ebf29f635cf7d
pritmanvar <pritmanvar@gmail.com>
2025-04-19 18:42:22 +0530

    added API for get basic analysis and post content.

--- agents.py
+++ agents.py
@@ -12,25 +12,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-analysis_llm_indx = 0
-analysis_llm = [
-    ChatGroq(
-        model="llama-3.1-8b-instant	",
-        temperature=0.8,
-        max_tokens=800,
-        timeout=None,
-        max_retries=2,
-        api_key=os.getenv("GROQ_ANALYSIS_API_KEY"),
-    ),
-    ChatGroq(
-        model="llama-3.1-8b-instant	",
-        temperature=0.8,
-        max_tokens=800,
-        timeout=None,
-        max_retries=2,
-        api_key=os.getenv("GROQ_ANALYSIS_API_KEY2"),
-    ),
-]
+analysis_llm = ChatGroq(
+    model="llama-3.1-8b-instant	",
+    temperature=0.8,
+    max_tokens=800,
+    timeout=None,
+    max_retries=2,
+    api_key=os.getenv("GROQ_ANALYSIS_API_KEY"),
+)
 post_content_llm = ChatGroq(
     model="qwen-qwq-32b",
     temperature=0.8,
@@ -39,16 +28,6 @@ post_content_llm = ChatGroq(
     api_key=os.getenv("THREADS_POST_GENERATION_API_KEY"),
 )
 
-class NewsObject(BaseModel):
-    title: str = Field(description="Title of the news")
-    url: str = Field(description="URL of the news")
-    description: str = Field(description="Description of the news")
-    time: str = Field(description="Time of the news")
-    source: str = Field(description="Source of the news")
-    will_it_directly_impact_any_stock: str = Field(description="Will it directly impact any Indian stock -> true/false")
-    directly_mentioned_companies_in_news: list = Field(description="List of companies mentioned in the news")
-    how_will_it_impact: str = Field(description="How will it impact")
-    reason: str = Field(description="Reason for the impact")
     
 def basic_analysis(news):
     global analysis_llm_indx
@@ -56,15 +35,12 @@ def basic_analysis(news):
         template_file="prompts/news_selector.yml",
         input_variables=["news_object"],
     )
-    # parser = JsonOutputParser(pydantic_object=NewsObject)
-
 
     for _ in range(5):
         try:
-            response = analysis_llm[analysis_llm_indx].invoke(
+            response = analysis_llm.invoke(
                 prompt.invoke({"news_object": news})
             )
-            analysis_llm_indx = (analysis_llm_indx + 1) % len(analysis_llm)
             print("################ BASIC ANALYSIS AGENT RESPONSE ################")
             print(response.content)
             print("################ BASIC ANALYSIS END AGENT RESPONSE ################")
--- app.py
+++ app.py
@@ -2,17 +2,14 @@ from fastapi import FastAPI
 
 from main import run_job
 from functions.threads_api import post_thread_with_text
+from agents import basic_analysis, get_text_post_content
 
 app = FastAPI()
 
-@app.get("/")
-def hello_world():
-    return {"data": "Welcome!!!"}
+@app.post("/get_basic_analysis")
+def handle_basic_analysis(news: dict):
+    return basic_analysis(news)
 
-@app.get("/run_job")
-def handle_job_running():
-    return run_job()
-
-@app.post("/post_thread")
-def handle_job_running(text: str):
-    return post_thread_with_text(text)
+@app.post("/get_text_post_content")
+def handle_text_post_content(details: str, reference: str):
+    return get_text_post_content(details, reference)
